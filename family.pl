9c117cd7caba4cd5ed5b4e8af2af01e67939b607
Maspriadi Dalimunthe <45424234+mamas182@users.noreply.github.com>
2018-12-07 20:04:51 +0700

    Create family.pl

---
+++ family.pl
@@ -0,0 +1,26 @@
+laki(husain).
+laki(abdul).
+laki(rudi).
+laki(amir).
+laki(basir).
+
+wanita(rita).
+wanita(tuti).
+wanita(siti).
+
+orangtua(rita, abdul).
+orangtua(rita, rudi).
+
+orangtua(husain, rudi).
+
+orangtua(rudi, tuti).
+orangtua(rudi, siti).
+
+orangtua(siti,amir).
+orangtua(siti, basir).
+
+
+anak(X,Y):-orangtua(Y,X).  
+
+
+nenek(X,Z):-orangtua(X,Y),orangtua(Y,Z),wanita(X).
